aa28e96b4ead222e970250dead09be2e6584d806
josecas <jmcg0901@gmail.com>
2022-06-30 21:17:15 -0700

    Initial Commit

---
+++ BandMate/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  BandMate
+//
+//  Created by Jose Castillo Guajardo on 6/30/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ BandMate/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  BandMate
+//
+//  Created by Jose Castillo Guajardo on 6/30/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
