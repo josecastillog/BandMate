61f58728dcacb092477c7bfbad855bf83c451535
josecas <jmcg0901@gmail.com>
2022-07-05 16:19:08 -0700

    Initialized Pods

---
+++ Pods/Target Support Files/Pods-BandMate-BandMateUITests/Pods-BandMate-BandMateUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BandMate_BandMateUITests : NSObject
+@end
+@implementation PodsDummy_Pods_BandMate_BandMateUITests
+@end
---
+++ Pods/Target Support Files/Pods-BandMate/Pods-BandMate-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BandMate : NSObject
+@end
+@implementation PodsDummy_Pods_BandMate
+@end
